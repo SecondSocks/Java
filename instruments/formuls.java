f65f3eaf0ed759690493ce360e688e271a2111b1
SecondSocks <hitrovalubov267@gmail.com>
2024-05-08 17:59:28 +0700

    Edit file formuls.java

--- instruments/formuls.java
+++ instruments/formuls.java
@@ -31,4 +31,47 @@ public class formuls {
         System.out.println(c);
     }
 
+    public static void squareOfSquare() {
+
+        System.out.print("Введите сторону квадрата: ");
+        double a = scan.nextDouble();
+
+        System.out.printf("\n%s", a*a);
+    }
+
+    public static void squareOfTriangle() {
+
+        System.out.print("Введите основание треугольника: ");
+        double a = scan.nextDouble();
+
+        System.out.print("\nВведите высоту треугольника: ");
+        double h = scan.nextDouble();
+
+        System.out.printf("\n%s", a / 2 * h);
+    }
+
+    public static void squareOfRectangle() {
+
+        System.out.print("Введите первую сторону прямогульника: ");
+        double a = scan.nextDouble();
+
+        System.out.print("\nВведите вторую сторону прямоугольника: ");
+        double b = scan.nextDouble();
+
+        System.out.printf("\n%s", a * b);
+    }
+
+    public static void squareOfTrapezoid() {
+
+        System.out.print("Введите первое основание трапеции: ");
+        double a = scan.nextDouble();
+
+        System.out.print("\nВведите второе основание трапеции: ");
+        double b = scan.nextDouble();
+
+        System.out.print("\nВведите высоту трапеции: ");
+        double h = scan.nextDouble();
+
+        System.out.printf("\n%s", (a+b) / 2 * h);
+    }
 }
